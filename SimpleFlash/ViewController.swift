473f53817260bebb7f57ed039fcb603ebc8436b6
MacBookPro <MacBookPro@MacBookProui-MacBook-Pro.local>
2016-12-22 10:26:15 +0900

    Torch Turn On/Off function added

--- SimpleFlash/ViewController.swift
+++ SimpleFlash/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet private var torchSwitch : UISwitch!
+    @IBOutlet private var torchInfo : UITextField!
+    private let avCaptureDevice = AVCaptureDevice.defaultDevice(withMediaType : AVMediaTypeVideo)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        torchSwitch.setOn(false, animated: false)
     }
-
+    
+    func ledStateChange(state : Bool){
+        if let avCaptureDevice = avCaptureDevice{
+            if avCaptureDevice.hasTorch{
+                do{
+                    try avCaptureDevice.lockForConfiguration()
+                    
+                    print(state)
+                    if state{
+                        avCaptureDevice.torchMode = .on
+                    }else{
+                        avCaptureDevice.torchMode = .off
+                    }
+                    
+                    avCaptureDevice.unlockForConfiguration()
+                }catch{
+                    torchInfo.text = "Un Error occured"
+                    return
+                }
+            }else{
+                torchInfo.text = "Current device doesn't have a LED Flash"
+                return
+            }
+        }
+    }
+    
+    @IBAction func switchDidEvent(sender : UISwitch){
+        ledStateChange(state: sender.isOn)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
